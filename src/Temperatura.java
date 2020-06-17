450ef7e4220222d62d0c67836520cb791ef5ec28
karabola <agardom@op.pl>
2020-06-17 21:55:49 +0200

    Add new files

---
+++ src/Temperatura.java
@@ -0,0 +1,11 @@
+public class Temperatura {
+    public static void main(String[] args) {
+        int temperature = -10;
+
+        if (temperature > 0) {
+            System.out.println("Temperatura jest dodatnia");
+        } else {
+            System.out.println("Temperatura jest ujemna");
+        }
+    }
+}
